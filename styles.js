e2fdfe3d3db91eb8a5b519d9fe55c949b3753d65
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-27 20:06:48 +0200

    Added the decreasing attempts on wrong answer

--- styles.js
+++ styles.js
@@ -1,6 +1,7 @@
 const answer = document.getElementById("answer");
 const done = document.getElementById("done");
 const blank = document.getElementById("word");
+const attempts = document.getElementById("try");
 
 const words = [
   "table",
@@ -18,19 +19,27 @@ const words = [
   "temperature",
 ];
 
+{
+  win = false;
+  found = false;
+}
+
 let rand = words[Math.floor(Math.random() * words.length)];
 for (let i = 0; i < rand.length; i++) {
   blank.innerHTML += `<div class="dash""><div class="blur"  id="blur-${i}">${rand[i]}</div></div>`;
 }
 
-console.log(blank);
 done.addEventListener("click", check);
 function check() {
-  console.log(answer.value);
   for (let i = 0; i < rand.length; i++) {
     if (answer.value == rand[i]) {
       const blurry = document.getElementById("blur-" + i);
       blurry.classList.remove("blur");
+      found = true;
     }
   }
+  if (found == false) {
+    attempts.innerHTML -= 1;
+  }
+  found = false;
 }
