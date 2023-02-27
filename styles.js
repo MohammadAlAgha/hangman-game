66dd7ba8053ec4419d13b2264ba3e5c9a486b6ad
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-27 16:50:40 +0200

    Added the words and saved a random

---
+++ styles.js
@@ -0,0 +1,20 @@
+const answer = document.getElementById("answer");
+
+const words = [
+  "table",
+  "apple",
+  "ball",
+  "banana",
+  "key",
+  "mouse",
+  "dinner",
+  "country",
+  "toothbrush",
+  "chair",
+  "hoodie",
+  "laptop",
+  "temperature",
+];
+
+let rand = words[Math.floor(Math.random() * words.length)];
+console.log(rand);
