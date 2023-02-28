b2c636f42fb550752fe96a7aed962acf762d02ef
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-28 08:55:22 +0200

    Added a restart button

--- styles.js
+++ styles.js
@@ -3,6 +3,7 @@ const done = document.getElementById("done");
 const blank = document.getElementById("word");
 const attempts = document.getElementById("try");
 const message = document.getElementById("message");
+const start = document.getElementById("start");
 
 const words = [
   "table",
@@ -31,45 +32,57 @@ const words = [
   "doctionaries",
 ];
 
-let rand = words[Math.floor(Math.random() * words.length)];
-for (let i = 0; i < rand.length; i++) {
-  blank.innerHTML += `<div class="dash""><div class="blur"  id="blur-${i}">${rand[i]}</div></div>`;
-}
+start.addEventListener("click", restart);
 
-const rules = {
-  found: false,
-  steps: rand.length,
-  game: true,
-};
-done.addEventListener("click", check);
-function check() {
-  if (answer.value.length == 1) {
-    if (rules.game) {
-      if (attempts.innerHTML != 0) {
-        for (let i = 0; i < rand.length; i++) {
-          if (answer.value == rand[i]) {
-            const blurry = document.getElementById("blur-" + i);
-            blurry.classList.remove("blur");
-            rules.found = true;
-            rules.steps -= 1;
+function restart() {
+  attempts.innerHTML = 7;
+  message.innerHTML = "";
+  blank.innerHTML = "";
+  steps = 0;
+
+  let rand = words[Math.floor(Math.random() * words.length)];
+  for (let i = 0; i < rand.length; i++) {
+    blank.innerHTML += `<div class="dash""><div class="blur"  id="blur-${i}">${rand[i]}</div></div>`;
+  }
+  const blur = document.getElementById("blur");
+  const rules = {
+    found: false,
+    steps: rand.length,
+    game: true,
+  };
+  done.addEventListener("click", check);
+  function check() {
+    if (answer.value.length == 1) {
+      if (rules.game) {
+        if (attempts.innerHTML != 0) {
+          for (let i = 0; i < rand.length; i++) {
+            if (answer.value == rand[i]) {
+              const blurry = document.getElementById("blur-" + i);
+              blurry.classList.remove("blur");
+              rules.found = true;
+              rules.steps -= 1;
+            }
           }
-        }
-        if (rules.steps == 0) {
-          message.innerHTML = "You won!!!";
-          rules.game = false;
-        }
-        if (rules.found == false) {
-          if (attempts.innerHTML != 1) {
-            attempts.innerHTML -= 1;
-          } else {
-            message.innerHTML = "You Lost :( ,  try again.";
+          if (rules.steps == 0) {
+            message.innerHTML = "You won!!!";
+            start.innerHTML = "Restart";
             rules.game = false;
           }
+          if (rules.found == false) {
+            if (attempts.innerHTML != 1) {
+              attempts.innerHTML -= 1;
+            } else {
+              message.innerHTML = "You Lost :( ,  try again.";
+              rules.game = false;
+            }
+          }
+          rules.found = false;
         }
-        rules.found = false;
       }
+    } else {
+      message.innerHTML = "Please enter one letter";
+      start.innerHTML = "Restart";
     }
-  } else {
-    message.innerHTML = "Please enter one letter";
   }
+  blur.innerHTML = null;
 }
